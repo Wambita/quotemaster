6582fde14a1e2a0e8c6422c2278984c6725ca8a5
Wambita Fana <wambitafana@gmail.com>
2019-03-27 11:28:31 +0300

    Add details to the quotes (author)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from   './quotes'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QUOTES';
+  quotes = [
+    new Quotes(1,'Dont say maybe if you want to say no'),
+    new Quotes(2,'Do not wait, the time will never be just right'), 
+    new Quotes(3,'I never dreamed about success.I worked towards it'), 
+    new Quotes(4,'It is going to be hard , but hard does not mean impossible'),
+  ]
+
 }
+

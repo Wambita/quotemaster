0e392e67af43a41579d2de67898ba4fa373fc9ce
Wambita Fana <wambitafana@gmail.com>
2019-03-27 12:28:54 +0300

    Add authors of the quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,11 @@ import { Quotes } from   './quotes'
 })
 export class AppComponent {
   title = 'QUOTES';
-  quotes = [
-    new Quotes(1,'Dont say maybe if you want to say no'),
-    new Quotes(2,'Do not wait, the time will never be just right'), 
-    new Quotes(3,'I never dreamed about success.I worked towards it'), 
-    new Quotes(4,'It is going to be hard , but hard does not mean impossible'),
-  ]
-
+  // quotes = [
+  //   new Quotes(1,'Do not say maybe if you want to say no','Ryan Holiday'),
+  //   new Quotes(2,'Do not wait, the time will never be just right','Napoleon Hill'), 
+  //   new Quotes(3,'I never dreamed about success.I worked towards it','Estee Lauder'),
+  //   new Quotes(4,'It is going to be hard , but hard does not mean impossible','Anonymous'),
+  // ]
 }
 
